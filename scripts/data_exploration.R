cdcb8a16eb62f39a6e07895c8514f494b8fca71b
Corbec <corbec023@gmail.com>
2022-01-06 20:44:34 -0600

    Figured out tidycensus

--- scripts/data_exploration.R
+++ scripts/data_exploration.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(tidycensus)
+library(jsonlite)
 
 options(scipen = 100)
 
@@ -13,33 +15,49 @@ year %>%
   ggplot(aes(x = lower_oop)) +
   geom_histogram()
 
+categories <- year %>% 
+  group_by(agg_cause) %>% 
+  count()
 
- 
-  year %>% 
-    filter(lower_oop == max(year$lower_oop))
+year %>% 
+  filter(lower_oop == max(year$lower_oop))
 
-  ed_visits %>% 
-    group_by(agg_cause) %>% 
-    filter(agg_cause == 'Communicable and nutrition disorders') %>% 
-    filter(!age_group_name == 'All Ages') %>% 
-    filter(sex == 'Both') %>% 
-    group_by(year_id) %>% 
-    ggplot(aes(x = year_id, y = lower_oop)) +
-    geom_point() +
-    geom_smooth()
+ed_visits %>% 
+  group_by(agg_cause) %>% 
+  filter(agg_cause == 'Communicable and nutrition disorders') %>% 
+  filter(!age_group_name == 'All Ages') %>% 
+  filter(sex == 'Both') %>% 
+  group_by(year_id) %>% 
+  ggplot(aes(x = year_id, y = lower_oop)) +
+  geom_point() +
+  geom_smooth()
 
-  year %>% 
-    group_by(agg_cause) %>% 
-    filter(agg_cause == 'Communicable and nutrition disorders') %>% 
-    filter(!age_group_name == 'All Ages') %>% 
-    filter(sex == 'Both') %>% 
-    filter(lower_oop < 10000000)
+year %>% 
+  group_by(agg_cause) %>% 
+  filter(agg_cause == 'Communicable and nutrition disorders') %>% 
+  filter(!age_group_name == 'All Ages') %>% 
+  filter(sex == 'Both') %>% 
+  filter(lower_oop < 10000000)
 # ^ age ranges with lowest oop are 65+. Most likely because of Medicare
-  
-  year %>% 
-    group_by(agg_cause) %>% 
-    filter(agg_cause == 'Communicable and nutrition disorders') %>% 
-    filter(!age_group_name == 'All Ages') %>% 
-    filter(sex == 'Both') %>% 
-    filter(lower_oop > 30000000)
-# ^ ages 1 to 4 have the highest oop, followed by 20 to 29 age ranges  
+
+year %>% 
+  group_by(agg_cause) %>% 
+  filter(agg_cause == 'Communicable and nutrition disorders') %>% 
+  filter(!age_group_name == 'All Ages') %>% 
+  filter(sex == 'Both') %>% 
+  filter(lower_oop > 30000000)
+# ^ ages 1 to 4 have the highest oop, followed by 20 to 29 age ranges 
+
+key <- fromJSON("../data/census_key.json")
+
+census_api_key(key)
+
+v2006 <- load_variables(2006, "acs1", cache = TRUE)
+
+pop_2006 <- get_acs(geography = "us",
+                    survey = "acs1",
+                    table = "B01001",
+                    year = "2006",
+                    show_call = TRUE)
+
+pop_2006 <- left_join(pop_2006, v2006, by = c("variable" = "name"))
