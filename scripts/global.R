1ea4052da8b3f2d448d2d83db89fedc143fb24c5
Corbec <corbec023@gmail.com>
2022-01-06 21:29:51 -0600

    working on cleaning census data, added app files

---
+++ scripts/global.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+library(tidycensus)
+library(jsonlite)
+
+ed_visits <- read_csv("../data/ED_spending/IHME_DEX_ED_SPENDING_2006_2016_DATA_Y2021M09D23.CSV")
+
---
+++ scripts/ui.R
@@ -0,0 +1,21 @@
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel(""),
+
+    # Sidebar 
+    sidebarLayout(
+        sidebarPanel(
+            
+        ),
+
+       
+        mainPanel(
+            
+        )
+    )
+))
