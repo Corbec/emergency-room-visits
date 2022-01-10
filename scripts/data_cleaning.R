9420005af137a16567c791f7abb2ec77492c3050
Corbec <corbec023@gmail.com>
2022-01-09 18:34:12 -0600

    Wrote function to clean census data and wrote finished file to rds

--- scripts/data_cleaning.R
+++ scripts/data_cleaning.R
@@ -58,3 +58,159 @@ pop_2006 <- pop_2006 %>%
   distinct()
 
 # Need to repeat for other groups and turn into a function to easily cover all 10 years
+
+
+
+
+
+#Building function to pull and clean census data
+
+
+census_cleaned <- function(date){
+  vars <- load_variables(date, "acs1", cache = FALSE)
+  
+  population <- get_acs(geography = "us",
+                      survey = "acs1",
+                      table = "B01001",
+                      year = toString(date),
+                      show_call = TRUE)
+  
+  population <- left_join(population, vars, by = c("variable" = "name"))
+  
+  population <- population %>% 
+    separate(label,
+             sep = "!!",
+             into = c("estimate!!", "total", "sex", "age_group")) %>% 
+    select(-c("estimate!!", "concept", "total", "moe", "GEOID", "NAME", "variable"))
+  
+  # aggregating males age 20-24
+  male_age_20_24 <- population %>% 
+    filter(sex == "Male" & (age_group == "20 years" | age_group == "21 years" | age_group == "22 to 24 years")) %>% 
+    mutate(age_group = "20 to 24") 
+  
+  male_age_20_24 <- aggregate(.~ sex + age_group, data = male_age_20_24, sum)
+  
+  #aggregating females age 20-24
+  female_age_20_24 <- population %>% 
+    filter(sex == "Female" & (age_group == "20 years" | age_group == "21 years" | age_group == "22 to 24 years")) %>% 
+    mutate(age_group = "20 to 24") 
+  
+  female_age_20_24 <- aggregate(.~ sex + age_group, data = female_age_20_24, sum)
+  
+  # aggregating males 15-19
+  male_age_15_19 <- population %>% 
+    filter(sex == "Male" & (age_group == "15 to 17 years" | age_group == "18 and 19 years")) %>% 
+    mutate(age_group = "15 to 19") 
+  
+  male_age_15_19 <- aggregate(.~ sex + age_group, data = male_age_15_19, sum)
+  
+  #aggregating females 15-19
+  female_age_15_19 <- population %>% 
+    filter(sex == "Female" & (age_group == "15 to 17 years" | age_group == "18 and 19 years")) %>% 
+    mutate(age_group = "15 to 19") 
+  
+  female_age_15_19 <- aggregate(.~ sex + age_group, data = female_age_15_19, sum)
+  
+  #aggregating males age 60-64
+  male_age_60_64 <- population %>% 
+    filter(sex == "Male" & (age_group == "60 and 61 years" | age_group == "62 to 64 years")) %>% 
+    mutate(age_group = "60 to 64") 
+  
+  male_age_60_64 <- aggregate(.~ sex + age_group, data = male_age_60_64, sum)
+  
+  #aggregating females age 60-64
+  female_age_60_64 <- population %>% 
+    filter(sex == "Female" & (age_group == "60 and 61 years" | age_group == "62 to 64 years")) %>% 
+    mutate(age_group = "60 to 64") 
+  
+  female_age_60_64 <- aggregate(.~ sex + age_group, data = female_age_60_64, sum)
+  
+  #aggregating males age 65-69
+  male_age_65_69 <- population %>% 
+    filter(sex == "Male" & (age_group == "65 and 66 years" | age_group == "67 to 69 years")) %>% 
+    mutate(age_group = "65 to 69") 
+  
+  male_age_65_69 <- aggregate(.~ sex + age_group, data = male_age_65_69, sum)
+  
+  #aggregating females age 65-69
+  female_age_65_69 <- population %>% 
+    filter(sex == "Female" & (age_group == "65 and 66 years" | age_group == "67 to 69 years")) %>% 
+    mutate(age_group = "65 to 69") 
+  
+  female_age_65_69 <- aggregate(.~ sex + age_group, data = female_age_65_69, sum)
+  
+  #adding each group to population tibble
+  population <- population %>% 
+    mutate(estimate = replace(estimate,
+                              ((age_group == "20 years" | age_group == "21 years" | age_group == "22 to 24 years") & sex == "Male"),
+                              male_age_20_24$estimate)) %>% 
+    mutate(age_group = replace(age_group,
+                               ((age_group == "20 years" | age_group == "21 years" | age_group == "22 to 24 years") & sex == "Male"),
+                               male_age_20_24$age_group)) %>% 
+    mutate(estimate = replace(estimate,
+                              ((age_group == "20 years" | age_group == "21 years" | age_group == "22 to 24 years") & sex == "Female"),
+                              female_age_20_24$estimate)) %>% 
+    mutate(age_group = replace(age_group,
+                               ((age_group == "20 years" | age_group == "21 years" | age_group == "22 to 24 years") & sex == "Female"),
+                               female_age_20_24$age_group)) %>% 
+    mutate(estimate = replace(estimate,
+                              ((age_group == "15 to 17 years" | age_group == "18 and 19 years") & sex == "Male"),
+                              male_age_15_19$estimate)) %>% 
+    mutate(age_group = replace(age_group,
+                               ((age_group == "15 to 17 years" | age_group == "18 and 19 years") & sex == "Male"),
+                               male_age_15_19$age_group)) %>% 
+    mutate(estimate = replace(estimate,
+                              ((age_group == "15 to 17 years" | age_group == "18 and 19 years") & sex == "Female"),
+                              female_age_15_19$estimate)) %>% 
+    mutate(age_group = replace(age_group,
+                               ((age_group == "15 to 17 years" | age_group == "18 and 19 years") & sex == "Female"),
+                               female_age_15_19$age_group)) %>% 
+    mutate(estimate = replace(estimate,
+                              ((age_group == "60 and 61 years" | age_group == "62 to 64 years") & sex == "Male"),
+                              male_age_60_64$estimate)) %>% 
+    mutate(age_group = replace(age_group,
+                               ((age_group == "60 and 61 years" | age_group == "62 to 64 years") & sex == "Male"),
+                               male_age_60_64$age_group)) %>% 
+    mutate(estimate = replace(estimate,
+                              ((age_group == "60 and 61 years" | age_group == "62 to 64 years") & sex == "MFem"),
+                              female_age_60_64$estimate)) %>% 
+    mutate(age_group = replace(age_group,
+                               ((age_group == "60 and 61 years" | age_group == "62 to 64 years") & sex == "Fem"),
+                               female_age_60_64$age_group)) %>% 
+    mutate(estimate = replace(estimate,
+                              ((age_group == "65 and 66 years" | age_group == "67 to 69 years") & sex == "Male"),
+                              male_age_65_69$estimate)) %>% 
+    mutate(age_group = replace(age_group,
+                               ((age_group == "65 and 66 years" | age_group == "67 to 69 years") & sex == "Male"),
+                               male_age_65_69$age_group)) %>% 
+    mutate(estimate = replace(estimate,
+                              ((age_group == "65 and 66 years" | age_group == "67 to 69 years") & sex == "Female"),
+                              female_age_65_69$estimate)) %>% 
+    mutate(age_group = replace(age_group,
+                               ((age_group == "65 and 66 years" | age_group == "67 to 69 years") & sex == "Female"),
+                               female_age_65_69$age_group))
+  
+  population <- population %>% 
+    distinct()
+  
+  population <- population %>% 
+    mutate(age_group = str_remove(population$age_group, "years")) %>% 
+    mutate(year = date)
+}
+
+#Loop to pull all years census data
+for(i in 2006:2016) { 
+  nam <- paste("pop", i, sep = "_")
+  assign(nam, census_cleaned(i))
+}
+
+#Combining all tibbles into one
+
+tibbleList <- list(pop_2006, pop_2007, pop_2008, pop_2009, pop_2010, pop_2011, pop_2012, pop_2013, pop_2014, pop_2015, pop_2016)
+
+pop_all <- bind_rows(tibbleList)
+
+#saving to RDS
+
+saveRDS(pop_all, file = '../data/cleaned_census.rds')
+  
